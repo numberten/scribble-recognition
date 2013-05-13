7a565f9aa433abc77ba4af8cace26536f8baa6be
jesse <number.dot.ten@gmail.com>
2013-05-12 23:52:38 -0700

    character capturing

--- image_processing/capture.py
+++ image_processing/capture.py
@@ -1,6 +1,6 @@
 import Image
 
-im = Image.open("images/arial_characters.png")
+im = Image.open("../images/arial_characters.png")
 #im = Image.open("images/Penguin03black.gif")
 
 x = im.size[0]
@@ -20,3 +20,35 @@ for i in range(0,y):
          s = s + ' '
       #print "("+str(j)+","+str(i)+") = " + str(im.getpixel((j,i)))
 print s
+
+def isEmptyPixel((x,y), img, emptyval = 0):
+   return img.getpixel((x,y)) == emptyval
+
+def getNeighbors((x,y), img):
+   neighbors = []
+   if (x > 0):
+      neighbors.append((x-1,y))
+   if (x < img.size[0]-1):
+      neighbors.append((x+1,y))
+   if (y > 0):
+      neighbors.append((x,y-1))
+   if (y < img.size[1]-1):
+      neighbors.append((x,y+1))
+   if (x > 0 and y > 0):
+      neighbors.append((x-1,y-1))
+   if (x > 0 and y < img.size[1]-1):
+      neighbors.append((x-1,y+1))
+   if (x < img.size[0]-1 and y > 0):
+      neighbors.append((x+1,y-1))
+   if (x < img.size[0]-1 and y < img.size[1]-1):
+      neighbors.append((x+1,y+1))
+      
+def getActiveNeighbors(xy, img):
+   return filter(lambda (x,y): not isEmptyPixel((x,y), img), getNeighbors(xy, img))
+
+
+#class Character:
+   
+#   def __init__(self, init):
+      #init.
+
