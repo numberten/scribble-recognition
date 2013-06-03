1ba408de64d2d55e40e52da359b2441b42f7727e
jesse <number.dot.ten@gmail.com>
2013-06-02 23:15:39 -0700

    random epsilon generated weights

--- image_processing/NN2.py
+++ image_processing/NN2.py
@@ -2,6 +2,7 @@ from random import random
 from numpy import *
 from ast import literal_eval
 from math import e
+from math import sqrt
 
 def sigmoid(x):
    return 1/(1+e**-x)
@@ -20,18 +21,28 @@ def nnCostFunction(hidden_layers, X, y, reg, epilson):
       print "Must have the same number of training inputs as outputs"
       return 0
    input_layer_size = X.shape[1]
-   print "Input Layer Size: " + str(input_layer_size)
    output_layer_size = y.shape[1]
    thetas = []
    prevSize = input_layer_size
    for i in range(0,len(hidden_layers)):
-      thetas.append(array([[0] * (hidden_layers[i] * (prevSize + 1))]))
+      eint = sqrt(6)/sqrt(prevSize + hidden_layers[i])
+      print "Eint: "+str(eint)
+      temp = []
+      for j in range(0,(hidden_layers[i] * (prevSize + 1))):
+         temp.append(random.random() * 2 * eint - eint)
+      thetas.append(array(temp))
       thetas[i].resize(hidden_layers[i],(prevSize + 1))
       prevSize = hidden_layers[i]
-   thetas.append(array([[0] * (output_layer_size * (prevSize + 1))]))
+   eint = sqrt(6)/sqrt(prevSize + output_layer_size)
+   print "Eint: "+str(eint)
+   temp = []
+   for j in range(0,output_layer_size * (prevSize + 1)):
+      temp.append(random.random() * 2 * eint - eint)
+   thetas.append(array(temp))
    thetas[len(thetas)-1].resize(output_layer_size,(prevSize + 1))
 
    print str(thetas)
+   #Theta numbers working properly
 
 #Xor gate testing, 4 reals
 print "Making a [2,1] XOR gate"
